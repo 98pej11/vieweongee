39a1286368f6e8aa0fd115a8d60ba241db0533e5
민동주 <ods1room@gmail.com>
2023-02-09 17:24:01 +0900

    feat : 채점표 생성, 채점표 불러오기 구현

--- frontend/src/store/meetingStore.js
+++ frontend/src/store/meetingStore.js
@@ -1,8 +1,4 @@
-import {
-  getInterviewOrder,
-  postScorecards,
-  getAllScorecards,
-} from "../api/meeting";
+import { getInterviewOrder, postScorecards, getAllScorecards } from "../api/meeting";
 
 const meetingStore = {
   namespaced: true,
@@ -72,25 +68,43 @@ const meetingStore = {
     },
   },
   actions: {
-    async getOrder({ commit, dispatch }, params) {
-      await getInterviewOrder(
-        params,
+    async makeScoreAndGetOrder({ commit, state }, params) {
+      // console.log("1!!!!! 액션진입");
+      //채점표를 생성한다
+      await postScorecards(
+        params.study_ID,
         ({ data }) => {
+          // console.log("방장님 채점표가 어떻게 됐을까요");
+          // console.log(data);
           if (data.message == "SUCCESS") {
-            //정상적으로 받아왔을 때 채점표 생성부터 처리
-            console.log("방장님 면접 순서 잘 받아왔는데 채점표부터 생성할게요");
-            dispatch("makeScorecards", params.study_ID);
-            //면접 순서 저장 처리
-            // commit("SET_INTERVIEW_ORDER", data.data);
-            commit("SET_LEADER_ORDER", data.data);
-            // console.log(data.data);
-            // console.log("리더의 순서 지정 성공 >> " + state.leaderOrder);
+            console.log("방장님 채점표가 생성됐어요");
           }
         },
         (error) => {
           console.log(error);
         }
       );
+      // console.log("2!!!!! 채점표생성완료");
+      //면접 순서를 받아온다
+      await getInterviewOrder(
+        params,
+        ({ data }) => {
+          if (data.message == "SUCCESS") {
+            //정상적으로 받아왔을 때 채점표 생성부터 처리
+            // console.log("방장님 면접 순서 잘 받아왔는데 채점표부터 생성할게요");
+            // dispatch("makeScorecards", params.study_ID);
+            //면접 순서 저장 처리
+            // commit("SET_INTERVIEW_ORDER", data.data);
+            commit("SET_LEADER_ORDER", data.data);
+            // console.log(data.data);
+            console.log("리더의 순서 지정 성공 했어요 >> " + state.leaderOrder);
+          }
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+      // console.log("3!!!!! 면접 순서 불러오고 저장 완료");
     },
     async makeScorecards(study_ID) {
       //채점표 생성하기
@@ -108,15 +122,19 @@ const meetingStore = {
         }
       );
     },
-    async getScorecards({ commit }, study_ID) {
+    async getScorecards({ commit, state }, study_ID) {
       //채점표 전부 가져오기
       await getAllScorecards(
         study_ID,
         ({ data }) => {
+          // console.log("채점표가 잘 가져와졌을까요");
+          // console.log(data);
           if (data.message == "SUCCESS") {
-            console.log("채점표 전부 가져왔어요");
-            console.log(data.data);
+            // console.log("채점표 전부 가져왔어요");
+            //   console.log(data.data);
             commit("SET_SCORE_LIST", data.data);
+            console.log("스코어리스트 보여드릴게요");
+            console.log(state.scoreList);
           }
         },
         (error) => {
@@ -135,7 +153,7 @@ const meetingStore = {
       //방장이 설정한 면접자 인원 설정
       commit("SET_INTERVIEWEERATE", cnt);
     },
-    setInterviewList({ commit }, order) {
+    setInterviewList({ commit, state }, order) {
       //총 회차 설정
       commit("SET_TOTALTURN", order.split("!")[0]);
       //현재 회차에 처음 회차 설정
@@ -165,8 +183,8 @@ const meetingStore = {
 
       //면접자의 아이디 리스트 저장
       commit("SET_INTERVIEW_ORDER_LIST", resultList);
-      // console.log("면접 리스트 >> ");
-      // console.log(resultList);
+      console.log("면접 리스트 >> ");
+      console.log(state.interviewOrderList);
     },
   },
 };
