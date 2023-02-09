db5e650624f3c08a686cc803c49f3e81a87d86be
박은정 <98pej11@gmail.com>
2023-02-10 08:53:06 +0900

    feat : modal 알림 추가

--- frontend/src/api/meeting.js
+++ frontend/src/api/meeting.js
@@ -8,11 +8,13 @@ async function getInterviewOrder(params, success, fail) {
   api.defaults.headers["ACCESS"] = sessionStorage.getItem("ACCESS");
   await api.post(`/meeting/${params.study_ID}/order`, params.data).then(success).catch(fail);
 }
+
 //채점표 생성
 async function postScorecards(study_ID, success, fail) {
   api.defaults.headers["ACCESS"] = sessionStorage.getItem("ACCESS");
   await api.post(`/meeting/${study_ID}/score`).then(success).catch(fail);
 }
+
 //채점표 가져오기
 async function getAllScorecards(study_ID, success, fail) {
   api.defaults.headers["ACCESS"] = sessionStorage.getItem("ACCESS");
