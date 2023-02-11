d1ffb096f662d1dc7a15c4cfd8b9551e821ebc50
duridudu <acd1026@naver.com>
2023-02-11 20:28:55 +0900

    feat : 임시저장

--- backend/src/main/java/com/ssafy/vieweongee/controller/StudyController.java
+++ backend/src/main/java/com/ssafy/vieweongee/controller/StudyController.java
@@ -40,8 +40,6 @@ public class StudyController {
     private final FileUploadService fileUploadService;
 
 
-
-
     /** 스터디 모집 게시글 생성
      * @param createStudyRequest
      * @return study_id
@@ -54,11 +52,11 @@ public class StudyController {
         User user = userService.getUserById(user_id);
         log.info("user in study : {}",user.getName());
 
-            // 스터디 생성
-            Study study = studyService.createStudy(user, createStudyRequest);
+        // 스터디 생성
+        Study study = studyService.createStudy(user, createStudyRequest);
 
-            // 참가 명단에 추가
-            studyService.registParticipant(user, study);
+        // 참가 명단에 추가
+        studyService.registParticipant(user, study);
 
         // 참가 이력 생성
         studyService.createProgress(user, study);
