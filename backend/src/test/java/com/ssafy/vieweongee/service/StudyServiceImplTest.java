1eda2d88c5747923cc94a153dec213087e5f99eb
duridudu <acd1026@naver.com>
2023-02-12 17:32:01 +0900

    feat : Add test code for StudyController

--- backend/src/test/java/com/ssafy/vieweongee/service/StudyServiceImplTest.java
+++ backend/src/test/java/com/ssafy/vieweongee/service/StudyServiceImplTest.java
@@ -1,4 +1,63 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.ssafy.vieweongee.service;
+
+import com.ssafy.vieweongee.dto.study.CreateStudyRequest;
+import com.ssafy.vieweongee.entity.Study;
+import com.ssafy.vieweongee.entity.User;
+import com.ssafy.vieweongee.repository.StudyRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
 class StudyServiceImplTest {
-  
+
+
+    @Test
+    void updateStudy() {
+    }
+
+    @Test
+    void deleteStudy() {
+    }
+
+    @Test
+    void getAllStudy() {
+    }
+
+    @Test
+    void getTop3Study() {
+    }
+
+    @Test
+    void getStudyDetail() {
+    }
+
+    @Test
+    void searchStudy() {
+    }
+
+    @Test
+    void registParticipant() {
+    }
+
+    @Test
+    void createProgress() {
+    }
+
+    @Test
+    void getParticipantCnt() {
+    }
+
+    @Test
+    void cancelStudy() {
+    }
+
+    @Test
+    void updateResume() {
+    }
 }
