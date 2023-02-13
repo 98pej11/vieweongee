21d41805b2d73fce37446b6ca2ee6b63db7cabd3
이리나 <nalee9801@naver.com>
2023-02-13 15:25:56 +0900

    feat : vue.config.js 수정

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,14 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// })
-
-module.exports = {
-  devServer: {
-    proxy: {
-      '/api': {
-        target: "https://vieweongee.kro.kr:8080/api",
-      },
-    },
-  },
-}
+module.exports = defineConfig({
+  transpileDependencies: true
+})
