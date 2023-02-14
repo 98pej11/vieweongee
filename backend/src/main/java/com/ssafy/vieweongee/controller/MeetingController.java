0fc5a518e442a7710247c95d155c9a6f6fad6693
함정빈 <shadberry02@naver.com>
2023-02-14 22:17:08 +0900

    feat : modify MeetingController

--- backend/src/main/java/com/ssafy/vieweongee/controller/MeetingController.java
+++ backend/src/main/java/com/ssafy/vieweongee/controller/MeetingController.java
@@ -172,10 +172,8 @@ public class MeetingController {
      * @return
      */
     @PutMapping("/{study_ID}/status")
-//    ResponseEntity<?> modifyConfirmAndStatus(@PathVariable("study_ID") String study_ID, @RequestHeader("ACCESS") String access){
-    ResponseEntity<?> modifyConfirmAndStatus(@PathVariable("study_ID") String study_ID, @RequestBody String id){
-//        Long userId = Long.parseLong(tokenService.getUid(access).replaceAll("\"",""));
-        Long userId = Long.parseLong(id);
+    ResponseEntity<?> modifyConfirmAndStatus(@PathVariable("study_ID") String study_ID, @RequestHeader("ACCESS") String access){
+        Long userId = Long.parseLong(tokenService.getUid(access).replaceAll("\"",""));
         //status, confirm 변경
         meetingService.updateConfirmAndStatus(Long.parseLong(study_ID), userId);
         // summary 변경
