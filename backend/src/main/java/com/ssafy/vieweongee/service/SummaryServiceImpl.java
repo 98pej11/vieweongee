0c0e90de205abb35e0a5d3f3189e59b00e8d78cb
duridudu <acd1026@naver.com>
2023-02-14 22:38:56 +0900

    refactor : Modify Summary table of Social User

--- backend/src/main/java/com/ssafy/vieweongee/service/SummaryServiceImpl.java
+++ backend/src/main/java/com/ssafy/vieweongee/service/SummaryServiceImpl.java
@@ -6,25 +6,41 @@ import com.ssafy.vieweongee.entity.User;
 import com.ssafy.vieweongee.repository.SummaryRepository;
 import com.ssafy.vieweongee.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
 @Slf4j
 @Service
+@Transactional
 public class SummaryServiceImpl implements SummaryService{
-    @Autowired
-    UserRepository userRepository;
-    @Autowired
-    SummaryRepository summaryRepository;
+
+
+    private final UserRepository userRepository;
+
+    private final SummaryRepository summaryRepository;
+
+    public SummaryServiceImpl(UserRepository userRepository, SummaryRepository summaryRepository) {
+        this.userRepository = userRepository;
+        this.summaryRepository = summaryRepository;
+    }
+
+//    public SummaryServiceImpl(UserRepository userRepository, SummaryRepository summaryRepository) {
+//        this.userRepository = userRepository;
+//        this.summaryRepository = summaryRepository;
+//    }
+
     @Override
     public void createSummary(UserCreateRequest user) {
         log.info("크리에이트 드러왔더요, 이메일 : {}, 프로바이더 : {}", user.getEmail(), user.getProvider());
 //        User dbUser=userRepository.findByEmailAndProvider(user.getEmail(), user.getProvider());
-        User dbUser = userRepository.getUserByEmailandSocial(user.getEmail(),"global");
-        log.info("DB user in summary {}",dbUser.getName());
+        User dbUser = userRepository.getUserByEmailandSocial(user.getEmail(),user.getProvider());
+        log.info("DB user in summary {} and {}",dbUser.getName(), dbUser.getId());
 
         Summary newSum;
         newSum = new Summary(dbUser);
-        log.info("---------{}---------",newSum.getId());
+        log.info("---------{}---------",newSum.getUser().getId());
+//        log.info(newSum.getId().toString());
         // 둘 중 하나로 하면 잘됩니당
         summaryRepository.save(newSum);
 //        Summary newSum = summaryRepository.save(Summary.builder().user(dbUser).build());
