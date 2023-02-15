6bb95fd7022eed9ecdd9a97ea089d7d7d2816d2e
민동주 <ods1room@gmail.com>
2023-02-15 13:14:08 +0900

    fix : 서버 설정

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 // local vue api axios instance
 export default axios.create({
-  // baseURL: "https://vieweongee.kro.kr:8080/api",
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://vieweongee.kro.kr:8080/api",
+  // baseURL: "http://localhost:8080/api",
   // baseURL: "https://jsonplaceholder.typicode.com",
   headers: {
     "Content-Type": "application/json;charset=utf-8",
